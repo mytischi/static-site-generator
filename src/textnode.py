10bc7d108f60fbf396203d56f4ce86b263b7d064
Matvei Naumov <naumov.mytischi@gmail.com>
2026-03-15 14:12:42 +0400

    Created base directories, main.sh and main.py

---
+++ src/textnode.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class TextType (Enum):
+    Text_TextType = "Text"
+    Bold_TextType = "Bold text"
+    Italic_TextType = "Italic text"
+    Code_TextType = "Code text"
+    Link_TextType = "Link"
+    Image_TextType = "Image"
