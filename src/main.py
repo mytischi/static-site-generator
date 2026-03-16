08cf16e7f32c24786cbac441c638d2e7deb0c765
Matvei Naumov <naumov.mytischi@gmail.com>
2026-03-16 23:39:46 +0400

    edited textnode.py, main.py, created TextType

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("hello, world")
+from textnode import TextNode, TextType
+
+
+def main():
+    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    
+    print(node)
+
+main()
