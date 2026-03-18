e7d4df525af4ef99d2f9d0915f5f210315e02b18
Matvei Naumov <naumov.mytischi@gmail.com>
2026-03-19 01:07:53 +0400

    updated unittest

---
+++ src/test_textnode.py
@@ -0,0 +1,23 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_TextType_notEq(self):
+        node = TextNode("this is a text node", TextType.BOLD)
+        node2 = TextNode("this is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+    def test_UrlIsNone(self):
+        node = TextNode("this is a text node", TextType.BOLD, url = None)
+        node2 = TextNode("this is a text node", TextType.BOLD, url = None)
+        self.assertEqual(node, node2)
+
+if __name__ == "__main__":
+    unittest.main()
